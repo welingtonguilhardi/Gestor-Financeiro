0561c31e86251caa32c74e3be0f45943a0109e50
Welington Argolo Guilhardi <welingtonpitbull123@gmail.com>
2024-05-07 18:29:08 -0300

    Colocando verificação se user está logado antes de acessar pagina home

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
+from audioop import reverse
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
+
+
+@login_required( login_url = 'logar' )
 def home (request):
     return HttpResponse('Pagina home')
